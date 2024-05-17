2eff9e61594addedfb7662c4f6e4a972a0f4dd99
Refactoring <544135240@qq.com>
2024-05-17 15:33:07 +0900

    Changed the HTML for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,7 @@
-
-from django.conf.urls import url
 from django.contrib import admin
-from django.conf.urls import include
+from django.urls import path, include
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'', include('blog.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
 ]
